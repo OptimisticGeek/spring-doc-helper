e8e6a6339a610e340f394d9f2114b1a9a4c1117e
OptimisticGeek <optimisticgeekone@gmail.com>
2024-02-07 21:17:44 +0800

    1.删除注释中的html标签

--- src/main/kotlin/com/github/optimisticgeek/spring/ext/PsiJavaDocumentExt.kt
+++ src/main/kotlin/com/github/optimisticgeek/spring/ext/PsiJavaDocumentExt.kt
@@ -4,14 +4,13 @@ package com.github.optimisticgeek.spring.ext
 import com.github.optimisticgeek.spring.constant.AUTHOR
 import com.github.optimisticgeek.spring.constant.PARAM
 import com.intellij.psi.PsiJavaDocumentedElement
-import org.apache.commons.lang3.StringUtils
 
 fun PsiJavaDocumentedElement.getDocumentTitle(): String {
-    return this.docComment?.descriptionElements?.getOrNull(1)?.text?.trim() ?: StringUtils.EMPTY
+    return this.docComment?.descriptionElements?.getOrNull(1)?.text.trim()
 }
 
 fun PsiJavaDocumentedElement.getDocumentTag(tag: String): String {
-    return this.docComment?.findTagByName(tag)?.valueElement?.text ?: StringUtils.EMPTY
+    return this.docComment?.findTagByName(tag)?.valueElement?.text.trim()
 }
 
 fun PsiJavaDocumentedElement.getDocumentTagParam(param: String): String {
@@ -21,8 +20,7 @@ fun PsiJavaDocumentedElement.getDocumentTagParam(param: String): String {
 fun PsiJavaDocumentedElement.getDocumentTagParam(tag: String, param: String): String {
     return this.docComment?.findTagsByName(tag)?.map { it.text }
         ?.firstOrNull { it.startsWith("@$tag $param ") }
-        ?.replace(Regex("^@$tag $param (.+?)\n.*?$"), "\$1")
-        ?: StringUtils.EMPTY
+        ?.replace(Regex("^@$tag $param (.+?)\n.*?$"), "\$1").trim()
 }
 
 fun PsiJavaDocumentedElement.getAuthor(): String {
@@ -31,4 +29,8 @@ fun PsiJavaDocumentedElement.getAuthor(): String {
 
 fun PsiJavaDocumentedElement.getRemark(): String {
     return this.getDocumentTitle()
+}
+
+private fun String?.trim(): String {
+    return this?.replace("\\s*<\\s*\\w+\\s*[^>]*>\\s*", "")?.trim() ?: ""
 }
